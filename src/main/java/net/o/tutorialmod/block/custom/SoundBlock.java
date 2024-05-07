7d443e8a7ec7b194ea71f5807ee4abf2f8b620bf
Oskar Jasiewicz <oskar.jasiewicz@ucdconnect.ie>
2024-05-07 20:03:44 +0100

    Added custom tooltips to the Metal Detector item and the Sound Block.

--- src/main/java/net/o/tutorialmod/block/custom/SoundBlock.java
+++ src/main/java/net/o/tutorialmod/block/custom/SoundBlock.java
@@ -1,10 +1,16 @@
 package net.o.tutorialmod.block.custom;
 
+import java.util.List;
+
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Item.TooltipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -20,5 +26,12 @@ public class SoundBlock extends Block {
             BlockHitResult pHitResult) {
         pLevel.playSound(pPlayer, pPos, SoundEvents.NOTE_BLOCK_DIDGERIDOO.get(), SoundSource.BLOCKS, 1f, 1f);
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTootipComponents,
+            TooltipFlag pTooltipFlag) {
+        pTootipComponents.add(Component.literal("Makes sounds when used"));
+        super.appendHoverText(pStack, pContext, pTootipComponents, pTooltipFlag);
     }
 }
--- src/main/java/net/o/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/o/tutorialmod/item/custom/MetalDetectorItem.java
@@ -1,5 +1,7 @@
 package net.o.tutorialmod.item.custom;
 
+import java.util.List;
+
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -7,6 +9,8 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -52,5 +56,12 @@ public class MetalDetectorItem extends Item {
 
     private boolean isValuableBlock(BlockState state) {
         return state.is(Blocks.IRON_ORE) || state.is(Blocks.DIAMOND_ORE);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents,
+            TooltipFlag pTooltipFlag) {
+        pTooltipComponents.add(Component.translatable("tooltip.tutorialmod.metal_detector.tooltip"));
+        super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
     }
 }
