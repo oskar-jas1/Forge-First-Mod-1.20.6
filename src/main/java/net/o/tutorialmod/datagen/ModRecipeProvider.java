8c1017dde271b41a69791e483ebcdc1bf53d627e
Oskar Jasiewicz <oskar.jasiewicz@ucdconnect.ie>
2024-05-11 15:10:34 +0100

    Formatted file

--- src/main/java/net/o/tutorialmod/datagen/ModRecipeProvider.java
+++ src/main/java/net/o/tutorialmod/datagen/ModRecipeProvider.java
@@ -21,8 +21,8 @@ import java.util.concurrent.CompletableFuture;
 
 public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
     private static final List<ItemLike> SAPPHIRE_SMELTABLES = List.of(ModItems.RAW_SAPPHIRE.get(),
-            ModBlocks.SAPPHIRE_ORE.get(), ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(), ModBlocks.NETHER_SAPPHIRE_ORE.get(),
-            ModBlocks.END_STONE_SAPPHIRE_ORE.get());
+            ModBlocks.SAPPHIRE_ORE.get(), ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(),
+            ModBlocks.NETHER_SAPPHIRE_ORE.get(), ModBlocks.END_STONE_SAPPHIRE_ORE.get());
 
     public ModRecipeProvider(PackOutput pOutput, CompletableFuture<HolderLookup.Provider> pRegistries) {
         super(pOutput, pRegistries);
@@ -30,10 +30,10 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
 
     @Override
     protected void buildRecipes(RecipeOutput pRecipeOutput) {
-        oreSmelting(pRecipeOutput, SAPPHIRE_SMELTABLES, RecipeCategory.MISC, ModItems.SAPPHIRE.get(), 0.25f, 200,
-                "sapphire");
-        oreBlasting(pRecipeOutput, SAPPHIRE_SMELTABLES, RecipeCategory.MISC, ModItems.SAPPHIRE.get(), 0.25f, 100,
-                "sapphire");
+        oreSmelting(pRecipeOutput, SAPPHIRE_SMELTABLES, RecipeCategory.MISC, ModItems.SAPPHIRE.get(), 0.25f,
+                200, "sapphire");
+        oreBlasting(pRecipeOutput, SAPPHIRE_SMELTABLES, RecipeCategory.MISC, ModItems.SAPPHIRE.get(), 0.25f,
+                100, "sapphire");
 
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.SAPPHIRE_BLOCK.get())
                 .pattern("SSS")
@@ -61,17 +61,17 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
     }
 
     protected static void oreSmelting(
-            RecipeOutput pRecipeOutput, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult,
-            float pExperience, int pCookingTime, String pGroup) {
-        oreCooking(pRecipeOutput, RecipeSerializer.SMELTING_RECIPE, SmeltingRecipe::new, pIngredients, pCategory,
-                pResult, pExperience, pCookingTime, pGroup, "_from_smelting");
+            RecipeOutput pRecipeOutput, List<ItemLike> pIngredients, RecipeCategory pCategory,
+            ItemLike pResult, float pExperience, int pCookingTime, String pGroup) {
+        oreCooking(pRecipeOutput, RecipeSerializer.SMELTING_RECIPE, SmeltingRecipe::new, pIngredients,
+                pCategory, pResult, pExperience, pCookingTime, pGroup, "_from_smelting");
     }
 
     protected static void oreBlasting(
-            RecipeOutput pRecipeOutput, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult,
-            float pExperience, int pCookingTime, String pGroup) {
-        oreCooking(pRecipeOutput, RecipeSerializer.BLASTING_RECIPE, BlastingRecipe::new, pIngredients, pCategory,
-                pResult, pExperience, pCookingTime, pGroup, "_from_blasting");
+            RecipeOutput pRecipeOutput, List<ItemLike> pIngredients, RecipeCategory pCategory,
+            ItemLike pResult, float pExperience, int pCookingTime, String pGroup) {
+        oreCooking(pRecipeOutput, RecipeSerializer.BLASTING_RECIPE, BlastingRecipe::new, pIngredients,
+                pCategory, pResult, pExperience, pCookingTime, pGroup, "_from_blasting");
     }
 
     private static <T extends AbstractCookingRecipe> void oreCooking(
@@ -87,12 +87,14 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
             String pSuffix) {
         for (ItemLike itemlike : pIngredients) {
             SimpleCookingRecipeBuilder
-                    .generic(Ingredient.of(itemlike), pCategory, pResult, pExperience, pCookingTime, pSerializer,
+                    .generic(Ingredient.of(itemlike), pCategory, pResult, pExperience, pCookingTime,
+                            pSerializer,
                             pRecipeFactory)
                     .group(pGroup)
                     .unlockedBy(getHasName(itemlike), has(itemlike))
                     .save(pRecipeOutput,
-                            TutorialMod.MOD_ID + ":" + getItemName(pResult) + pSuffix + "_" + getItemName(itemlike));
+                            TutorialMod.MOD_ID + ":" + getItemName(pResult) + pSuffix + "_"
+                                    + getItemName(itemlike));
         }
     }
 }
